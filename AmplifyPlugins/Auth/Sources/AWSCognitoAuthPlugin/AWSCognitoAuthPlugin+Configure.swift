c8920070173e6df25af78bcdf7c3efb09989fe90
Chase Lawrence <lawrence_kirvon1@hotmail.com>
2025-07-01 18:45:01 +0200

    Fix WKWebView logout issues

--- AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/AWSCognitoAuthPlugin+Configure.swift
+++ AmplifyPlugins/Auth/Sources/AWSCognitoAuthPlugin/AWSCognitoAuthPlugin+Configure.swift
@@ -155,7 +155,7 @@ extension AWSCognitoAuthPlugin {
     }
 
     private func makeHostedUISession() -> HostedUISessionBehavior {
-        return HostedUISFSafariViewController()
+        return HostedUIWKWebViewAuthenticationSession()
     }
 
     private func makeURLSession() -> URLSession {
